3aa9039c5d1bc2bc4e1960e735722676ce3a1b7c
Andrea Grillo <andrea.grillo@epfl.ch>
2025-12-11 08:49:06 -0500

    stop tracking vitis output folder

--- vitis_projects/forward_subst/forward_subst/hls/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,5 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "forward_substitution", "RefName" : "forward_substitution","ID" : "0","Type" : "pipeline",
-"SubLoops" : [
-	{"Name" : "VITIS_LOOP_12_1","RefName" : "VITIS_LOOP_12_1","ID" : "1","Type" : "pipeline"},]
-}]}
